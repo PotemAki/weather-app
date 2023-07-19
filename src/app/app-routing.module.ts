944bef2722d5df762c7f2d35b9795025a2eac3da
PotemAki <michalmatyka194@gmail.com>
2023-07-19 16:19:11 +0200

    prod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/weather-app', pathMatch: 'full'},
+  { path: "weather-app", component: AppComponent },
+  { path: '**', redirectTo: '/weather-app'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
